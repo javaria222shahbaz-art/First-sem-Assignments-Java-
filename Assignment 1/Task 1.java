f85286628f3803aca8b61d5fa90ed6e647e0b252
Javaria Shahbaz <javaria222shahbaz@gmail.com>
2026-07-26 22:56:51 +0500

    Add files via upload

---
+++ Assignment 1/Task 1.java
@@ -0,0 +1,9 @@
+/**
+ * Project: First_Exercise
+ * Program: Display a text message to the console explaining the setup process.
+ */
+public class first_program {
+    public static void main(String[] args) {
+        System.out.println("This is my First Program");
+    }
+}
