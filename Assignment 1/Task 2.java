6af193cedc4458661e2578ce07367f7bea379f90
Javaria Shahbaz <javaria222shahbaz@gmail.com>
2026-07-26 22:57:25 +0500

    Add files via upload

---
+++ Assignment 1/Task 2.java
@@ -0,0 +1,18 @@
+public class second_exercise {
+    public static void main(String[] args) {
+        // This line displays the student's name
+        System.out.println("Name: Javaria");
+        
+        // This line displays the enrollment number
+        System.out.println("Enrollment: 02-135252-013");
+        
+        // This line displays the subject name
+        System.out.println("Subject: Object Oriented Programming");
+        
+        // This line displays the lab instructor's name
+        System.out.println("Lab instructor: Ms Madiha");
+        
+        // This line displays the current semester details
+        System.out.println("Semester: 2nd semester BS IT 2 (Fall 2025)");
+    }
+}
